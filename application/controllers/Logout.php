66e346ba52d05207444a1064ab32bab91081cf7f
erikprakoso <erikprakoso1993@gmail.com>
2021-06-10 17:30:21 +0700

    logout

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -7,6 +7,7 @@ class Logout extends CI_Controller
     public function index()
     {
         $this->session->unset_userdata('name');
+        $this->session->unset_userdata('username');
         $this->session->unset_userdata('email');
         $this->session->unset_userdata('user_id');
         $this->session->unset_userdata('logged_in');
